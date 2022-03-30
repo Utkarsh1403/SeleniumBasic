56c6fc8eb701de74312fc6f5406663256f482fb1
Utkarsh1403 <utkarshsontakke123@gmail.com>
2022-03-30 15:16:56 +0530

    updated

--- test_googlesearch.py
+++ test_googlesearch.py
@@ -6,6 +6,11 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 print("test case started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
+time.sleep(1)
+driver.find_element_by_name("q").send_keys("Harman")
+
+
+
 time.sleep(5)
 driver.close()
 print("Test case has successfully completed")
